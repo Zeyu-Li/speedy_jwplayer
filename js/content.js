56f97f8a098068653da2ca14ea33d72bb843b04d
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-03-01 22:36:31 -0700

    fixed and working

--- js/content.js
+++ js/content.js
@@ -1,6 +1,20 @@
+
 try {
-    var video = document.querySelector('.jw-video');
-    video.playbackRate=2;
+    let i = 0;
+    var wait = setInterval(function() {
+        if (document.querySelector('.jw-video') !== null) {
+            var video = document.querySelector('.jw-video');
+            video.playbackRate=2;
+            clearInterval(checkExist);
+        } else {
+            i++;
+        }
+        if (i > 100) {
+            // if 20 seconds have passed, forget about it
+            clearInterval(checkExist);
+        }
+
+    }, 200);
 } catch (err) {
     console.log("No JW player detected");
 }
